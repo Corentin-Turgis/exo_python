4654655a98de85da6cd0e0986e1cbaab06a57d84
Corentin Turgis <corentin.turgis@stayopen.io>
2025-02-05 21:02:56 +0100

    user test in board.py

--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,12 @@
 from Python.packages.cards_game import Board, Player, Card
 
+tao = Player('Tao')
+
 table1 = Board()
 table1.player_join(Player('Corentin'))
 table1.player_join(Player('Flora'))
 table1.player_join(Player('Rico'))
 table1.player_join(Player('Thibaut'))
-table1.player_join(Player('Tao'))
+table1.player_join(tao)
 
 table1.start_game()
-
-for player in table1.players:
-    print(f'{player.pseudo} :')
-    Card.display_cards(player.hand)
--- Python/packages/cards_game/board.py
+++ Python/packages/cards_game/board.py
@@ -1,5 +1,6 @@
 import random
 
+from .card import Card
 from .exceptions import DuplicateUserException
 from .deck import Deck
 from .player import Player
@@ -17,28 +18,42 @@ class Board:
         print(f'{player} join the table')
 
     def player_quit(self, player: Player):
-        if player in self.players:
-            for card in player.hand:
-                self.deck.add_card(card)
+        for card in player.hand:
+            self.deck.add_card(card)
         self.players.remove(player)
         print(f'{player} quit the table')
 
-    def player_draw(self, player):
-        if len(self.deck.cards) <= 0:
-            print(f'Sorry {player.pseudo} ! No more cards in this deck :(')
-            return
-        card = self.deck.draw()
-        player.add_card_to_hand(card)
-        print(f'{player} draw a card from {self.deck}')
+    def deal_parts_to_players(self, parts):
+        i = 0
+        while i < len(parts):
+            self.players[i].hand.update(parts[i])
+            i += 1
+
+
+    # def player_draw(self, player):
+    #     if len(self.deck.cards) <= 0:
+    #         print(f'Sorry {player.pseudo} ! No more cards in this deck :(')
+    #         return
+    #     card = self.deck.draw()
+    #     player.add_card_to_hand(card)
+    #     print(f'{player} draw a card from {self.deck}')
 
     def start_game(self):
         players_nb = len(self.players)
         parts = self.deck.deal_deck(players_nb)
         random.shuffle(self.players)
-        i = 0
-        while i < players_nb:
-            self.players[i].hand = parts[i]
-            i += 1
+        self.deal_parts_to_players(parts)
+        for player in self.players:
+            print(f'{player.pseudo} :')
+            Card.display_cards(player.hand)
+        self.player_quit(self.players[2])
+        parts = self.deck.deal_deck(len(self.players))
+        random.shuffle(parts)
+        self.deal_parts_to_players(parts)
+        for player in self.players:
+            print(f'{player.pseudo} :')
+            Card.display_cards(player.hand)
+
 
     def __repr__(self):
         return f'Board(Players: {[player.pseudo for player in self.players]}, {self.deck}'
